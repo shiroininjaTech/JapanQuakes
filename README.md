c581492bb7bd0ede11fbcb7602546a18407318b5
shiroininjaTech <47760502+shiroininjaTech@users.noreply.github.com>
2022-11-18 05:24:48 -0500

    Version 0.75 Release
    I've finally completed work on Version 0.75 of JapanQuakes.
    This upload contains many fixes from the original project, including better data cleansing and formatting.
    The project now has an install script and read me borrowed from AstroNinja

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # JapanQuakes
-A python program that scrapes basic earthquake data from the web and displays it using PyQt5
+JapanQuakes is a simple app written in Python and uses Pqyqt5 for a GUI. The app scrapes Earthquake data from the web using Beautiful Soup and dispays it to the user in a clean UI with graphs from MatPlotLib.
+
+
+## Installation
+
+### Debian-based Distros And Fedora
+Linux Mint/Ubuntu/Pop!OS
+
+- Open the terminal and clone this repository to your directory of choice:
+```bash
+cd ~/Downloads/
+git clone https://github.com/shiroininjaTech/JapanQuakes.git
+cd JapanQuakes/
+```
+- Use chmod to change the permissions of the installation file and run it:
+```bash
+chmod +x install.sh
+./install.sh
+```
+
+- Change username to your username in "icon" and "application" lines in the AstroNinja.desktop file using nano or a text editor:
+
+```bash
+nano ~/Desktop/JapanQuakes.desktop
+```
+
+
+### Use AstroNinja
+Ubuntu and Pop users: right-click on JapanQuakes desktop icon and click "Allow Launching".
+- It can also be launched from the terminal:
+
+```bash
+python3 JapanQuakes.py
+```
+## License
+
+This software is licensed under the GPLv3 license. 
+
---
+++ install.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+# A simple Bash shell script that installs packages depended on by JapanQuakes
+# Created by: Tom Mullins
+# Created: 11/18/2022
+# Modified: 11/18/2022
+
+
+# Testing for addition of an option to install on Fedora
+
+# Getting which distro the user is running
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+
+#echo $OS
+
+# If the user is running Fedora
+if [ "$OS" = "Fedora Linux" ] ; then
+  # First, we need to install the proper python 3 Libraries
+  sudo dnf install -y python3-pip python3-qt5 python3-dateutil python3-setuptools
+
+  # Next we install the libaries installed by pip
+  python3 -m pip install matplotlib lxml 
+
+  # removes the folder, then copies the files to a . folder.
+  rm -rf /home/$USER/.JapanQuakes
+  mkdir /home/$USER/.JapanQuakes
+
+  cp -r ./* /home/$USER/.JapanQuakes
+
+  # moving the desktop shortcut to the desktop
+  mv /home/$USER/.JapanQuakes/JapanQuakes.desktop /home/$USER/Desktop/
+
+  # Making both the desktop file and AstroNinjaMain.py executable
+  chmod +x /home/$USER/Desktop/JapanQuakes.desktop
+  chmod +x /home/$USER/.JapanQuakes/JapanQuakes.py
+
+else
+  # First, we need to install the proper python 3 Libraries
+  sudo apt-get install -y python3-pip python3-pyqt5 python3-dateutil python3-setuptools
+
+  # Next we install the libaries installed by pip
+  python3 -m pip install matplotlib lxml 
+
+  # removes the folder, then copies the files to a . folder.
+  rm -rf /home/$USER/.JapanQuakes
+  mkdir /home/$USER/.JapanQuakes
+
+  cp -r ./* /home/$USER/.JapanQuakes
+
+  # moving the desktop shortcut to the desktop
+  mv /home/$USER/.JapanQuakes/JapanQuakes.desktop /home/$USER/Desktop/
+
+  # Making both the desktop file and AstroNinjaMain.py executable
+  chmod +x /home/$USER/Desktop/JapanQuakes.desktop
+  chmod +x /home/$USER/.JapanQuakes/JapanQuakes.py
+
+fi
+
+  #echo $OS
+  # If the user is running Linux Mint
+  if [ "$OS" = "Linux Mint" ] ; then
+      # Removing an uneeded package that causes formatting errors in Linux Mint
+      sudo apt-get remove qt5ct ;
+  fi
+
+
+
+
+
+fi
